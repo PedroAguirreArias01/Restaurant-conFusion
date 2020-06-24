10bb05f847ad623b5426308eaf32e5350f0d82b3
Pedro Aguirre Arias <pedroaguirrearias01@gmail.com>
2020-06-24 10:59:22 -0500

    add dish and comments for details of dish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,38 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarBase } from '@angular/material/toolbar';
+import { MatToolbarModule } from '@angular/material/toolbar'; 
 import { FlexLayoutModule } from '@angular/flex-layout';
 import 'hammerjs';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { MenuComponent } from './menu/menu.component';
+import { MatListModule } from '@angular/material/list';
+import {MatCardModule} from '@angular/material/card';
+
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatButtonModule } from '@angular/material/button';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DishdetailComponent,
+    MenuComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule, 
     BrowserAnimationsModule,
-    MatToolbarBase, 
-    FlexLayoutModule
+    MatToolbarModule,
+    FlexLayoutModule,
+    MatListModule,
+    MatCardModule,
+    MatGridListModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/comment.ts
@@ -0,0 +1,9 @@
+/**the this class is the comment
+ * @author Pedro Aguirre Arias
+ */
+export class Comment {
+    rating: number;
+    comment: string;
+    author: string;
+    date: string;
+}
