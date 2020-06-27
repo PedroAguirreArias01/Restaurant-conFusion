63993116e16c65bbffd7ef53b7c73d039ba34ca4
Pedro Aguirre Arias <pedroaguirrearias01@gmail.com>
2020-06-27 09:36:56 -0500

    Angular Router Basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,10 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatButtonModule } from '@angular/material/button';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
@@ -23,7 +27,10 @@ import { FooterComponent } from './footer/footer.component';
     DishdetailComponent,
     MenuComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule, 
@@ -33,7 +40,8 @@ import { FooterComponent } from './footer/footer.component';
     MatListModule,
     MatCardModule,
     MatGridListModule,
-    MatButtonModule
+    MatButtonModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
